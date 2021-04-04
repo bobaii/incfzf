7cbbe547feb63a355f828b0a23cb7b0dfac5cf82
7iibob <7ii.bob.allen@protonmail.com>
2021-04-04 04:06:31 -0400

    master: small changes

--- incfzf.sh
+++ incfzf.sh
@@ -3,18 +3,21 @@
 path=$(echo -e "/\n`echo $HOME`" |fzf)
 selection=''
 
-while [[ ! $1 == '**flag**' ]]; do
+while [[ ! $1 == '**breakflag**' ]]; do
 
 [[ -n $selection ]] && 	path="$path/$selection"
 
+# use ctrl-s to break when at desired dir
 	
 	if [[ $(find $path -maxdepth 1 -type d |
 		wc -l   ) == 1 ]]; then break; fi
 
-	selection=$(ls  $path | fzf --bind "ctrl-s:execute(echo '**flag**')")
+	selection=$(ls  $path | fzf --bind "ctrl-s:execute(echo '**breakflag**')")
 
 	set -- $selection
 	
 done
 
 echo $path
+
+
